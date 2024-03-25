69fe9c28a848e01776c7adff36ea78aa10fa6d37
NathanSt10 <nstout@uwm.edu>
2024-03-24 23:31:38 -0500

    added multiple round functionality

--- app.js
+++ app.js
@@ -33,14 +33,44 @@ function playerWins(playerChoice, computerChoice) {
 
 function playRound(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
-        return "It's a Draw! You both threw " + playerChoice;
+        return 0//"It's a Draw! You both threw " + playerChoice;
     }
     else if (playerWins(playerChoice, computerChoice)) {
-        return "You Win! " + playerChoice + " beats " + computerChoice;
+        return 1//"You Win! " + playerChoice + " beats " + computerChoice;
     }
     else {
-        return "You Lose! " + computerChoice + " beats " + playerChoice;
+        return -1//"You Lose! " + computerChoice + " beats " + playerChoice;
     }
 }
 
-console.log(playRound(getPlayerChoice(), getComputerChoice()));
+function playGame(rounds) {
+    let draw = win = loss = 0;
+    for (let i = 0; i < rounds; ++i) {
+        let playerChoice = getPlayerChoice()
+        let computerChoice = getComputerChoice();
+        outcome = playRound(playerChoice, computerChoice);
+        if (outcome === 0) {
+            console.log("It's a Draw! You both threw " + playerChoice);
+            ++draw;
+        }
+        if (outcome === 1) {
+            console.log("You Win! " + playerChoice + " beats " + computerChoice);
+            ++win;
+        }
+        if (outcome === -1) {
+            console.log("You Lose! " + computerChoice + " beats " + playerChoice);
+            ++loss;
+        }
+    }
+    if (win > loss) {
+        console.log("Congrats you won! You beat the computer " + win + " to " + loss + "!")
+    }
+    else if (win === loss) {
+        console.log("The game concluded but there are no winners, you both drew... " + draw + " time(s)...")
+    }
+    else {
+        console.log("Darn you lost " + win + " to " + loss + ", better luck next time!")
+    }
+}
+
+playGame(parseInt(prompt("How many rounds would you like to play? ")));

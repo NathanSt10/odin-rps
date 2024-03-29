b637e054fdeaf8f7dce723a7d57b8001cbab8396
NathanSt10 <nstout@uwm.edu>
2024-03-29 17:47:35 -0500

    Added functionality to dislay the winner of the game

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let playerCount = computerCount = 0;
 
 function getComputerChoice() {
     switch(Math.floor(Math.random() * 3)) {
@@ -13,12 +14,6 @@ function getComputerChoice() {
     }
 }
 
-// function getPlayerChoice() {
-//     let choice = prompt("Rock, Paper, Scissors, Shoot!");
-//     choice = choice.toLowerCase();
-//     return choice[0].toUpperCase() + choice.slice(1);
-// }
-
 function playerWins(playerChoice, computerChoice) {
     if (playerChoice === "Rock") {
         return (computerChoice === "Scissors");
@@ -33,61 +28,40 @@ function playerWins(playerChoice, computerChoice) {
 
 function playRound(playerChoice, computerChoice) {
     const body = document.querySelector("body");
-    const div = document.querySelector("div");
-    // const displayResult = document.createElement("div");
+    const divRoundResult = document.querySelector("#roundResult");
+    const divButtonContainer = document.querySelector(".button-container");
+    console.log(divButtonContainer);
+    
     if (playerChoice === computerChoice) {
-        div.textContent = "It's a Draw! You both threw " + playerChoice;
+        divRoundResult.textContent = "It's a Draw! You both threw " + playerChoice;
     }
     else if (playerWins(playerChoice, computerChoice)) {
-        div.textContent = "You Win! " + playerChoice + " beats " + computerChoice;
+        divRoundResult.textContent = "You Win! " + playerChoice + " beats " + computerChoice;
+        ++playerCount;
     }
     else {
-        div.textContent = "You Lose! " + computerChoice + " beats " + playerChoice;
+        divRoundResult.textContent = "You Lose! " + computerChoice + " beats " + playerChoice;
+        ++computerCount;
     }
-    console.log(div);
-    body.appendChild(div);
+    console.log(divRoundResult);
+    console.log(playerCount);
+    console.log(computerCount);
+    body.appendChild(divRoundResult);
+    const divGameResult = document.querySelector("#gameResult");
+    body.appendChild(divGameResult);
+    if (playerCount === 5) {
+        divGameResult.textContent = "Congrats You Win! " + playerCount + " to " + computerCount + "!";
+    }
+    if (computerCount === 5) {
+        divGameResult.textContent = "Sorry you lost " + playerCount + " to " + computerCount + ", better luck next time!";
+    }
 }
 
 const btnRock = document.querySelector("#btn-rock");;
-
 btnRock.addEventListener("click", () => {playRound("Rock", getComputerChoice());});
 
 const btnPaper = document.querySelector("#btn-paper");
-
 btnPaper.addEventListener("click", () => {playRound("Paper", getComputerChoice());});
 
 const btnScissors = document.querySelector("#btn-scissors");
-
 btnScissors.addEventListener("click", () => {playRound("Scissors", getComputerChoice());});
-
-// function playGame(rounds) {
-//     let draw = win = loss = 0;
-//     for (let i = 0; i < rounds; ++i) {
-//         let playerChoice = getPlayerChoice()
-//         let computerChoice = getComputerChoice();
-//         outcome = playRound(playerChoice, computerChoice);
-//         if (outcome === 0) {
-//             console.log("It's a Draw! You both threw " + playerChoice);
-//             ++draw;
-//         }
-//         if (outcome === 1) {
-//             console.log("You Win! " + playerChoice + " beats " + computerChoice);
-//             ++win;
-//         }
-//         if (outcome === -1) {
-//             console.log("You Lose! " + computerChoice + " beats " + playerChoice);
-//             ++loss;
-//         }
-//     }
-//     if (win > loss) {
-//         console.log("Congrats you won! You beat the computer " + win + " to " + loss + "!")
-//     }
-//     else if (win === loss) {
-//         console.log("The game concluded but there are no winners, you both drew... " + draw + " time(s)...")
-//     }
-//     else {
-//         console.log("Darn you lost " + win + " to " + loss + ", better luck next time!")
-//     }
-// }
-
-// playGame(parseInt(prompt("How many rounds would you like to play? ")));

55fec40946e9e094252c9982a3e4c49e4c4268c1
NathanSt10 <nstout@uwm.edu>
2024-03-29 11:57:49 -0500

    Game is now played through primitive ui

--- app.js
+++ app.js
@@ -13,64 +13,81 @@ function getComputerChoice() {
     }
 }
 
-function getPlayerChoice() {
-    let choice = prompt("Rock, Paper, Scissors, Shoot!");
-    choice = choice.toLowerCase();
-    return choice[0].toUpperCase() + choice.slice(1);
-}
+// function getPlayerChoice() {
+//     let choice = prompt("Rock, Paper, Scissors, Shoot!");
+//     choice = choice.toLowerCase();
+//     return choice[0].toUpperCase() + choice.slice(1);
+// }
 
 function playerWins(playerChoice, computerChoice) {
     if (playerChoice === "Rock") {
-        return (computerChoice === "Scissors")
+        return (computerChoice === "Scissors");
     }
     else if (playerChoice === "Paper") {
-        return (computerChoice === "Rock")
+        return (computerChoice === "Rock");
     }
     else {
-        return (computerChoice === "Paper")
+        return (computerChoice === "Paper");
     }
 }
 
 function playRound(playerChoice, computerChoice) {
+    const body = document.querySelector("body");
+    const div = document.querySelector("div");
+    // const displayResult = document.createElement("div");
     if (playerChoice === computerChoice) {
-        return 0//"It's a Draw! You both threw " + playerChoice;
+        div.textContent = "It's a Draw! You both threw " + playerChoice;
     }
     else if (playerWins(playerChoice, computerChoice)) {
-        return 1//"You Win! " + playerChoice + " beats " + computerChoice;
+        div.textContent = "You Win! " + playerChoice + " beats " + computerChoice;
     }
     else {
-        return -1//"You Lose! " + computerChoice + " beats " + playerChoice;
+        div.textContent = "You Lose! " + computerChoice + " beats " + playerChoice;
     }
+    console.log(div);
+    body.appendChild(div);
 }
 
-function playGame(rounds) {
-    let draw = win = loss = 0;
-    for (let i = 0; i < rounds; ++i) {
-        let playerChoice = getPlayerChoice()
-        let computerChoice = getComputerChoice();
-        outcome = playRound(playerChoice, computerChoice);
-        if (outcome === 0) {
-            console.log("It's a Draw! You both threw " + playerChoice);
-            ++draw;
-        }
-        if (outcome === 1) {
-            console.log("You Win! " + playerChoice + " beats " + computerChoice);
-            ++win;
-        }
-        if (outcome === -1) {
-            console.log("You Lose! " + computerChoice + " beats " + playerChoice);
-            ++loss;
-        }
-    }
-    if (win > loss) {
-        console.log("Congrats you won! You beat the computer " + win + " to " + loss + "!")
-    }
-    else if (win === loss) {
-        console.log("The game concluded but there are no winners, you both drew... " + draw + " time(s)...")
-    }
-    else {
-        console.log("Darn you lost " + win + " to " + loss + ", better luck next time!")
-    }
-}
+const btnRock = document.querySelector("#btn-rock");;
 
-playGame(parseInt(prompt("How many rounds would you like to play? ")));
+btnRock.addEventListener("click", () => {playRound("Rock", getComputerChoice());});
+
+const btnPaper = document.querySelector("#btn-paper");
+
+btnPaper.addEventListener("click", () => {playRound("Paper", getComputerChoice());});
+
+const btnScissors = document.querySelector("#btn-scissors");
+
+btnScissors.addEventListener("click", () => {playRound("Scissors", getComputerChoice());});
+
+// function playGame(rounds) {
+//     let draw = win = loss = 0;
+//     for (let i = 0; i < rounds; ++i) {
+//         let playerChoice = getPlayerChoice()
+//         let computerChoice = getComputerChoice();
+//         outcome = playRound(playerChoice, computerChoice);
+//         if (outcome === 0) {
+//             console.log("It's a Draw! You both threw " + playerChoice);
+//             ++draw;
+//         }
+//         if (outcome === 1) {
+//             console.log("You Win! " + playerChoice + " beats " + computerChoice);
+//             ++win;
+//         }
+//         if (outcome === -1) {
+//             console.log("You Lose! " + computerChoice + " beats " + playerChoice);
+//             ++loss;
+//         }
+//     }
+//     if (win > loss) {
+//         console.log("Congrats you won! You beat the computer " + win + " to " + loss + "!")
+//     }
+//     else if (win === loss) {
+//         console.log("The game concluded but there are no winners, you both drew... " + draw + " time(s)...")
+//     }
+//     else {
+//         console.log("Darn you lost " + win + " to " + loss + ", better luck next time!")
+//     }
+// }
+
+// playGame(parseInt(prompt("How many rounds would you like to play? ")));

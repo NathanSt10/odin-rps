a9d0f6db38ad650c6b8ee928c7b7e2991ab86dbe
NathanSt10 <nstout@uwm.edu>
2024-03-29 19:21:53 -0500

    Added CSS using flex to layout components

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 let playerCount = computerCount = 0;
 
+const btnRock = document.querySelector("#btn-rock");;
+btnRock.addEventListener("click", () => {playRound("Rock", getComputerChoice());});
+
+const btnPaper = document.querySelector("#btn-paper");
+btnPaper.addEventListener("click", () => {playRound("Paper", getComputerChoice());});
+
+const btnScissors = document.querySelector("#btn-scissors");
+btnScissors.addEventListener("click", () => {playRound("Scissors", getComputerChoice());});
+
+const body = document.querySelector("body");
+const divRoundResult = document.querySelector("#roundResult");
+const divButtonContainer = document.querySelector(".button-container");
+const divGameResult = document.querySelector("#gameResult");
+const score = document.querySelector("#score");
+
+
 function getComputerChoice() {
     switch(Math.floor(Math.random() * 3)) {
         case 0:
@@ -27,28 +43,21 @@ function playerWins(playerChoice, computerChoice) {
 }
 
 function playRound(playerChoice, computerChoice) {
-    const body = document.querySelector("body");
-    const divRoundResult = document.querySelector("#roundResult");
-    const divButtonContainer = document.querySelector(".button-container");
-    console.log(divButtonContainer);
-    
+
     if (playerChoice === computerChoice) {
         divRoundResult.textContent = "It's a Draw! You both threw " + playerChoice;
     }
     else if (playerWins(playerChoice, computerChoice)) {
         divRoundResult.textContent = "You Win! " + playerChoice + " beats " + computerChoice;
         ++playerCount;
+        score.textContent = "" + playerCount + " - " + computerCount;
     }
     else {
         divRoundResult.textContent = "You Lose! " + computerChoice + " beats " + playerChoice;
         ++computerCount;
+        score.textContent = "" + playerCount + " - " + computerCount;
     }
-    console.log(divRoundResult);
-    console.log(playerCount);
-    console.log(computerCount);
-    body.appendChild(divRoundResult);
-    const divGameResult = document.querySelector("#gameResult");
-    body.appendChild(divGameResult);
+    
     if (playerCount === 5) {
         divGameResult.textContent = "Congrats You Win! " + playerCount + " to " + computerCount + "!";
     }
@@ -56,12 +65,3 @@ function playRound(playerChoice, computerChoice) {
         divGameResult.textContent = "Sorry you lost " + playerCount + " to " + computerCount + ", better luck next time!";
     }
 }
-
-const btnRock = document.querySelector("#btn-rock");;
-btnRock.addEventListener("click", () => {playRound("Rock", getComputerChoice());});
-
-const btnPaper = document.querySelector("#btn-paper");
-btnPaper.addEventListener("click", () => {playRound("Paper", getComputerChoice());});
-
-const btnScissors = document.querySelector("#btn-scissors");
-btnScissors.addEventListener("click", () => {playRound("Scissors", getComputerChoice());});
